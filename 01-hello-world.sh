7f922900800f4bd43f93e9f33b5c22f9061683e3
mohan <mohanprasad390@gmail.com>
2025-10-21 21:14:12 +0530

    message

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+NUMBER=$1
 
-echo "We no need declare the variable, shell will provide variable t us : $1"
-echo "No of variables passed to the script : $@"
-echo "Script Name : $0"
-echo "Current directory : $PWD"
-echo "User running the script : $USER"
-echo "Home directory of the user : $HOME"
-echo "PID of the script : $$"
-sleep 100 &
-echo "PID of the last command in background : $!"
+if [ $NUMBER -lt 10 ]
+then
+    echo "Given Number is less than 10"
+else
+    echo "Given Number is not less than 10"
+fi
+  
