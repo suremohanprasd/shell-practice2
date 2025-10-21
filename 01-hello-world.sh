80cb9e003f34ea8b8a3138a6dfcec936488bca28
mohan <mohanprasad390@gmail.com>
2025-10-21 19:27:26 +0530

    message

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo "I am Mohan Prasad Sure"
-echo "I am from Vijayawada"
+echo "Mohan  :: Hi Prasad"
+echo "Prasad :: Hi Mohan, How are you?"
+echo "Mohan  :: Iam fine Mohan, What about you"
+echo "Prasad :: Iam fine too, what are the plans for weekend?"
+echo "Mohan  :: Iam palnning to visit the temple with friends"
+echo "Prasad :: Nice, carry on."
