27df9c640660201d09f0e63011185744c8392820
mohan <mohanprasad390@gmail.com>
2025-10-21 20:18:04 +0530

    message

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -4,16 +4,6 @@ TIMESTAMP=$(date)
 
 echo "Script run at the time of $TIMESTAMP"
 
-echo "Please enter first number"
+NAMES=("Mohan,Sandeep,Sai,Sumanth,Ashok")
 
-read N1
-
-echo "Please enter second number"
-
-read N2
-
-echo "Addition of two NUM1 and NUM2 is : $(($N1+$N2))"
-
-echo "Subtraction of N1 and N2 is : $(($N1-$N2))"
-
-echo "Multiplication of N1 and N2 is $(($N1*$N2))"
+echo "First name is : ${NAMES[0]}"
