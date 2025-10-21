71b5c870f023360bf01006a273a5e233b387448e
mohan <mohanprasad390@gmail.com>
2025-10-21 19:30:52 +0530

    message

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo "Mohan  :: Hi Prasad"
-echo "Prasad :: Hi Mohan, How are you?"
-echo "Mohan  :: Iam fine Mohan, What about you"
-echo "Prasad :: Iam fine too, what are the plans for weekend?"
-echo "Mohan  :: Iam palnning to visit the temple with friends"
-echo "Prasad :: Nice, carry on."
+PERSON1=Mohan
+PERSON2=Prasad
+
+echo "$PERSON1  :: Hi $PERSON2"
+echo "$PERSON2 :: Hi $PERSON1, How are you?"
+echo "$PERSON1  :: Iam fine $PERSON1, What about you"
+echo "$PERSON2 :: Iam fine too, what are the plans for weekend?"
+echo "$PERSON1  :: Iam palnning to visit the temple with friends"
+echo "$PERSON2 :: Nice, carry on."
