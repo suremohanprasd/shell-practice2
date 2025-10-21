dc4a44740758c6f274ad5abea772699a6be0a26c
mohan <mohanprasad390@gmail.com>
2025-10-21 20:20:53 +0530

    message

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -7,3 +7,7 @@ echo "Script run at the time of $TIMESTAMP"
 NAMES=("Mohan" "Sandeep" "Sai" "Sumanth" "Ashok")
 
 echo "First name is : ${NAMES[0]}"
+echo "Second name is : ${NAMES[1]}"
+echo "Third name is : ${NAMES[2]}"
+echo "Fouth name is : ${NAMES[3]}"
+echo "Fifth name is : ${NAMES[4]}"
